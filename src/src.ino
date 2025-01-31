daa5eacef844a4c3bdcebaae0d41e82059b68b87
MatiasG19 <m.galusek@gmail.com>
2025-01-31 16:26:57 +0100

    fix: compenstate encoder inaccuracy

--- src/src.ino
+++ src/src.ino
@@ -8,36 +8,46 @@
 #define ENCODER_STEPS 4
 
 ClickEncoder *encoder;
-int16_t last, value;
+int16_t last, value, lastEncoderValue;
+unsigned long previousMillis = 0;
 
 void timerIsr() {
   encoder->service();
 }
 
 void setup() {
-  Serial.begin(9600)
+  Serial.begin(9600);
   Consumer.begin();
   encoder = new ClickEncoder(ENCODER_DT, ENCODER_CLK, ENCODER_SW);
 
-  Timer1.initialize(1000);
+  Timer1.initialize(100);
   Timer1.attachInterrupt(timerIsr); 
   last = -1;
 } 
 
 void loop() {  
+  // Encoder
   value += encoder->getValue();
-
-  if (value != last) {
-    if((value - last) >= ENCODER_STEPS) {
-      last = value; 
-      Consumer.write(MEDIA_VOLUME_UP);
-    }
-    else if((last - value) >= ENCODER_STEPS) {
-      last = value; 
-      Consumer.write(MEDIA_VOLUME_DOWN);
-    }
+  if((value - last) >= ENCODER_STEPS) {
+    last = value; 
+    Consumer.write(MEDIA_VOLUME_UP);
+  }
+  else if((last - value) >= ENCODER_STEPS) {
+    last = value; 
+    Consumer.write(MEDIA_VOLUME_DOWN);
   }
 
+  // Compensate encoder inaccuracy
+  if (lastEncoderValue == value) {
+    unsigned long currentMillis = millis(); 
+    if(currentMillis - previousMillis >= 500) {
+      previousMillis = currentMillis;
+      last = value;
+    }
+  }
+  lastEncoderValue = value;
+
+  // Button
   ClickEncoder::Button b = encoder->getButton();
   if (b != ClickEncoder::Open) {
     switch (b) {
